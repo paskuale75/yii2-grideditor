870fbb426a7fc6d4c5ebf22a09cd788509ba2b5e
paskuale <pellicanipasquale@gmail.com>
2019-07-19 11:24:42 +0200

    stkevich\yii2-ckeditor5 asset file changed

--- GrideditorAsset.php
+++ GrideditorAsset.php
@@ -20,7 +20,7 @@ class GrideditorAsset extends AssetBundle
         'yii\web\JqueryAsset',
         'yii\jui\JuiAsset',
         'marqu3s\summernote\SummernoteAsset',
-        'stkevich\ckeditor5\InlineAssets'
+        'stkevich\ckeditor5\ClassicAssets'
     ];
 
     public function __construct($config = [])
